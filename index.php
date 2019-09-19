a27a20ce8b25f37821d6e9b78efadd418af567b7
mitchell <mitchellvictoriano@gmail.com>
2019-09-19 07:20:16 -0700

    order

--- index.php
+++ index.php
@@ -33,10 +33,10 @@
         <a  href="#about">About</a>
       </li>
       <li>
-        <a  href="#projects">Projects</a>
+        <a  href="#skills">Skills</a>
       </li>
       <li>
-        <a  href="#skills">Skills</a>
+        <a  href="#projects">Projects</a>
       </li>
       <li>
         <a href="/resume">Resume</a>
