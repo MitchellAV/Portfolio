f9558251f2514019985c0249324ee5ec7bb57e1a
mitchell <mitchellvictoriano@gmail.com>
2019-09-19 05:50:09 -0700

    img

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
 <body>
   <header>
-    <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" width="100%" height="auto">
+    <img src="/imgs/hero-img.jpg" alt="">
   </header>
   <nav>
     <ul id="navbar">
@@ -49,30 +49,33 @@
       <a href="https://myanimelist.net/profile/MitchellV" target="_blank">MyAnimeList</a> |
       <a href="https://memrise.com/user/MitchellV/" target="_blank">Memrise</a>
     </h2>
-
+    <hr>
   </aside>
 
   <main id="container">
-    <h1>Work in Progress...</h1>
+    <h1>Work in Progress...</h1><hr>
 
     <section id="about">
       <h1>About me</h1>
       <p>I recently graduated from San Jose State University with a B.S. in Mechanical Engineering and two minors in Computer Science and Mathematics respectively. I am interested in Thermofluids and Computer Science and wish to pursue a career in
         either of these fields. I enjoy learning new skills and am passionate about technology and learning new languages. I am currently studying Japanese and Full-Stack Development in my spare time to further my knowledge and pursue my many
         interests. </p>
+<hr>
     </section>
 
     <section id="skills">
       <h1>Skills</h1>
+      <hr>
     </section>
 
     <section id="projects">
       <h1>Projects</h1>
-      <h1>Mechanical Engineering</h1>
+      <h2>Mechanical Engineering</h2>
+      <hr>
       <section id="me-projects">
 
         <article class="me-article">
-          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" height="100" width="100" class="article-img">
+          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" class="article-img">
           <div class="article-desc">
             <h2>Biomass Fuel Synthesizer Subsystems</h3>
               <p>San Jose State University Mechanical Engineering senior project.</p>
@@ -84,7 +87,7 @@
           <button type="button" name="button" class="live-view"><a href="https://drive.google.com/drive/folders/1ABje_NoLvo1Eb6C6gPmI-YtA4ywT66pm">View project</a></button>
         </article>
         <article class="me-article">
-          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" height="100" width="100" class="article-img">
+          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" class="article-img">
           <div class="article-desc">
             <h2>Shower Heat Exchanger</h3>
               <p>Verified the design of a horizontal double pipe heat exchanger presented in a research paper by using ANSYS Fluent (CFD Software) and comparing our results to their experimental temperature data. This home application of a heat
@@ -93,7 +96,7 @@
           <button type="button" name="button" class="live-view"><a href="https://docs.google.com/presentation/d/1VgQ5eYra02JxKyfxeS2wEvWehOgLRvz0Ecfv94kUz6g/edit#slide=id.p">View project</a></button>
         </article>
         <article class="me-article">
-          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" height="100" width="100" class="article-img">
+          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" class="article-img">
           <div class="article-desc">
             <h2>Ball and Plate PID Controller</h3>
               <p>Prototyped a Ball Centering Table which would balance a foam ball in the center of a platform that would correct itself upon any external disturbances. This project used four ultrasonic sensors, two light dependent resistor (LDR)
@@ -102,7 +105,7 @@
           <button type="button" name="button" class="live-view"><a href="https://www.dropbox.com/home/Projects/Ball%20Balancing%20Table">View project</a></button>
         </article>
         <article class="me-article">
-          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" height="100" width="100" class="article-img">
+          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" class="article-img">
           <div class="article-desc">
             <h2>Foldable Compact Step</h3>
               <p>Designed and prototyped a foldable step using SOLIDWORKS that fits flush in the toe space under kitchen cabinets and allows for more than half a foot increase in height when in use. The prototype was made from Aluminum, Birch wood,
@@ -110,13 +113,14 @@
           </div>
           <button type="button" name="button" class="live-view"><a href="https://www.youtube.com/watch?v=pX9qEwXQB0Q&feature=youtu.be">View project</a></button>
         </article>
-
       </section>
-      <h1>Computer Science</h1>
+      <hr>
+      <h2>Computer Science</h2>
+      <hr>
       <section id="cs-projects">
 
         <article class="cs-article">
-          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" height="100" width="100" class="article-img">
+          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" class="article-img">
           <div class="article-desc">
             <h2>FFXIII-2 Clock Puzzle Solver</h3>
               <p>Web application that solves the "Hands of Time" puzzle from the game Final Fantasy XIII-2.</p>
@@ -126,7 +130,7 @@
         </article>
 
         <article class="cs-article">
-          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" height="100" width="100" class="article-img">
+          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" class="article-img">
           <div class="article-desc">
             <h2>Woosh Yelp App</h3>
               <p>Web application that searches the users location for places to eat/drink and allows for a "Tinder" style swipe left or right decision.</p>
@@ -136,7 +140,7 @@
         </article>
 
         <article class="cs-article">
-          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" height="100" width="100" class="article-img">
+          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" class="article-img">
           <div class="article-desc">
             <h2>IMDb TV Graph</h3>
               <p>This web applications allows for the user to search for a TV show and creates a visual representation of each episode rating for each season as well as statistical information including trends and best/worst episodes and seasons.</p>
@@ -146,7 +150,7 @@
         </article>
 
         <article class="cs-article">
-          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" height="100" width="100" class="article-img">
+          <img src="https://i.imgur.com/GHHwIqP.jpg" alt="" class="article-img">
           <div class="article-desc">
             <h2>72 Seasons</h3>
               <p>This web applications displays the current microseason based from the traditional Japanese calendar. The traditional Japanese calendar contains 24 major divisions which are split into 3 minor divisions that marks the passing of the
@@ -156,6 +160,7 @@
           <button type="button" name="button" class="view-code">View Code</button>
         </article>
       </section>
+      <hr>
     </section>
 
   </main>
@@ -164,10 +169,11 @@
 
   <footer id="footer">
     <h2>Contact Me: <a href="mailto: mitchellvictoriano@gmail.com">mitchellvictoriano@gmail.com</a></h2>
-    <a href="https://www.linkedin.com/in/mitchellvictoriano/" target="_blank">LinkedIn</a>
-    <a href="https://github.com/MitchellAV" target="_blank">Github</a>
-    <a href="https://myanimelist.net/profile/MitchellV" target="_blank">MyAnimeList</a>
-    <a href="https://memrise.com/user/MitchellV/" target="_blank">Memrise</a>
+    <h2>Social: <a href="https://www.linkedin.com/in/mitchellvictoriano/" target="_blank">LinkedIn</a> |
+      <a href="https://github.com/MitchellAV" target="_blank">Github</a> |
+      <a href="https://myanimelist.net/profile/MitchellV" target="_blank">MyAnimeList</a> |
+      <a href="https://memrise.com/user/MitchellV/" target="_blank">Memrise</a>
+    </h2>
   </footer>
 </body>
 
